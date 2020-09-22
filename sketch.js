1e65a97e86bb005488e706ab66ec56574563b8f0
jsingavarapu <68450876+jsingavarapu@users.noreply.github.com>
2020-09-21 18:18:43 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,56 +1,57 @@
-//Create variables here
-var pet,dog,happyDog,database,foodS,foodStock
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
 
-function preload()
-{
-  //load images here
-  dog=loadImage("images/dogImg.png");
-  happyDog=loadImage("images/dogImg1.png");
-  
-}
+function preload(){
+   dogImg=loadImage("images/dogImg.png");
+   dogImg1=loadImage("images/dogImg1.png");
+  }
 
+//Function to set initial environment
 function setup() {
   database=firebase.database();
   createCanvas(500,500);
-  pet=createSprite(200,200,10,10);
-pet.addImage(dog);
-pet.scale=0.15;
- foodStock=database.ref('Food');
-  foodStock.on("value",readStock) 
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
 }
 
-
-function draw() {  
-background(46,139,87)
-if(keyWentDown(UP_ARROW)){
-  writeStock(foodS);
-  pet.addImage(happyDog);
-}
-
-  drawSprites();
-  //add styles here
-  textSize(20);
-  fill("orange");
-  text("Food left"+foodS,50,100);
-}
-  //Function to read values from Data Base
-  function readStock(data){
-    foodS=data.val();
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
   }
 
+  drawSprites();
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
 
-  //Function to write values in Data Base
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
 function writeStock(x){
   if(x<=0){
     x=0;
   }else{
     x=x-1;
-  }
+  } 
   database.ref('/').update({
     Food:x
   })
 }
-
-
-
-
+  
